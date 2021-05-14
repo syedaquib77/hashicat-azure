ebc51bb5c82542d1b89315dbc4d4815f6d0beef0
Syed Aquib <syedaquib77@hotmail.com>
2021-05-14 10:54:28 +0000

    commit vnet

---
+++ vnet.tf
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/syedaquib-training/network/azurerm"
+  version = "3.0.1"
+
+  resource_group_name = "${resource_group_name}"
+}
