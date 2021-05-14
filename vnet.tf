7a12d0aa980ca9b520e9abbe940ae8e80320cad1
Syed Aquib <syedaquib77@hotmail.com>
2021-05-14 10:58:29 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -4,5 +4,14 @@ module "network" {
   source  = "app.terraform.io/syedaquib-training/network/azurerm"
   version = "3.0.1"
 
-  resource_group_name = "${resource_group_name}"
+  resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    Billable    = "true"
+    Department  = "devops"
+    environment = "Production"
+  }
+}
 }
